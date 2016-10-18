307ec208750ea8fd915e61b414c6c6fa37208ffa
jock-dalby <jock-dalby@jock-dalby.local>
2016-10-19 06:43:56 +1300

    cat-api

---
+++ promises/callbacks.js
@@ -0,0 +1,56 @@
+/********** Callbacks ***************/
+
+const catAPI = 'http://thecatapi.com/api/images/get?format=src&type=gif'
+
+let addImg = (src) => {
+  let imgElement = document.createElement('img')
+  imgElement.src = src
+  document.getElementById('callbacks').appendChild(imgElement)
+}
+
+function loadImage(url, callback) {
+  let image = new Image()
+
+  image.onload = function() {
+    callback(null, image)
+  }
+
+  image.onerror = function() {
+    let msg = 'Could not load image at ' + url
+    callback(new Error(msg))
+  }
+
+  image.src = url
+}
+
+
+loadImage('images/cat1.jpg', (err, img1) => {
+  if(err) throw err;
+  addImg(img1.src)
+  loadImage('images/cat2.jpg', (err, img2) => {
+    if(err) throw err;
+    addImg(img2.src)
+    loadImage('images/cat3.jpg', (err, img3) => {
+      if(err) throw err;
+      addImg(img3.src)
+    })
+  })
+})
+
+// could this be map() ??
+
+let arr = [
+  'images/cat1.jpg',
+  'images/cat2.jpg',
+  'images/cat3.jpg'
+  ]
+
+arr.map(img => loadImage(img, (err, img1) => {
+  if(err) throw err;
+  addImg(img1.src)
+}))
+
+
+// not running in parallel, one must finish before next can start
+
+// What if we use promises ???
--- promises/promises.js
+++ promises/promises.js
@@ -1,53 +1,10 @@
-/********** Callbacks ***************/
-
-function loadImageCB(url, callback) {
-  let image = new Image()
-
-  image.onload = function() {
-    callback(null, image)
-  }
-
-  image.onerror = function() {
-    let msg = 'Could not load image at ' + url
-    callback(new Error(msg))
-  }
-
-  image.src = url
-}
-
-
-loadImageCB('images/cat1.jpg', (err, img1) => {
-  if(err) throw err;
-  addImgCB(img1.src)
-  loadImageCB('images/cat2.jpg', (err, img2) => {
-    if(err) throw err;
-    addImgCB(img2.src)
-    loadImageCB('images/cat3.jpg', (err, img3) => {
-      if(err) throw err;
-      addImgCB(img3.src)
-    })
-  })
-})
-
-// could this be map() ??
-
-let arr = [
-  'images/cat1.jpg',
-  'images/cat2.jpg',
-  'images/cat3.jpg'
-  ]
-
-arr.map(img => loadImageCB(img, (err, img1) => {
-  if(err) throw err;
-  addImgCB(img1.src)
-}))
-
-
-// not running in parallel, one must finish before next can start
-
-// What if we use promises ???
-
 /********** Promises ***************/
+
+let addImgPR = (src) => {
+  let imgElement = document.createElement('img')
+  imgElement.src = src
+  document.getElementById('promises').appendChild(imgElement)
+}
 
 function loadImagePR(url) {
   return new Promise((resolve, reject) => {
@@ -72,12 +29,11 @@ function loadImagePR(url) {
 // 2. This function promise function be called with two arguments, resolve and reject.
 // 3. Both resolve (success) and reject (failure) are also functions and which are called //    with the value or error, if and  when you have them.
 
-
-loadImagePR('images/cat1.jpg').then((img1) => {
+loadImagePR(catAPI).then((img1) => {
   addImgPR(img1.src)
-  loadImagePR('images/cat2.jpg').then((img2) => {
+  loadImagePR(catAPI).then((img2) => {
     addImgPR(img2.src)
-    loadImagePR('images/cat3.jpg').then((img3) => {
+    loadImagePR(catAPI).then((img3) => {
       addImgPR(img3.src)
     })
   })
@@ -86,9 +42,9 @@ loadImagePR('images/cat1.jpg').then((img1) => {
 // Promise.all()
 
 Promise.all([
-  loadImagePR('images/cat1.jpg'),
-  loadImagePR('images/cat2.jpg'),
-  loadImagePR('images/cat3.jpg')
+  loadImagePR(catAPI),
+  loadImagePR(catAPI),
+  loadImagePR(catAPI)
 ]).then((images) => {
   images.forEach(img => addImgPR(img.src))
 }).catch((err) => {
@@ -105,17 +61,3 @@ Promise.all([
 // 4.If an error occurs in any of functions called in promise.all, it will be caught by .catch, which means we do not need to handle the error in multiple places.
 
 // 5.Promise.all is just one example of this and the promise object has alot more functionality to offer.
-
-// addImg functions
-
-let addImgCB = (src) => {
-  let imgElement = document.createElement('img')
-  imgElement.src = src
-  document.getElementById('callbacks').appendChild(imgElement)
-}
-
-let addImgPR = (src) => {
-  let imgElement = document.createElement('img')
-  imgElement.src = src
-  document.getElementById('promises').appendChild(imgElement)
-}
